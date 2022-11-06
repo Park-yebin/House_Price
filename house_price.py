db91d37166034677145b579a2367093ce2deec1a
Park-yebin <81156273+Park-yebin@users.noreply.github.com>
2022-11-06 17:00:27 +0900

    Check dependent variable
    dependent variable==SalePrice

--- house_price.py
+++ house_price.py
@@ -9,3 +9,19 @@ import warnings
 
 warnings.filterwarnings('ignore')
 # %matplotlib inline
+
+# bring data
+df_train=pd.read_csv('House_Price/data/train.csv')
+
+# Check the decoration
+df_train.columns
+
+# descriptive statistics summary
+df_train['SalePrice'].describe
+
+# get histogram
+sns.distplot(df_train['SalePrice'])
+
+# skewness and kurtosis
+print("Skewness: %f" % df_train['SalePrice'].skew())
+print("Kurtosis: %f" % df_train['SalePrice'].kurt())
