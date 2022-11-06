80ff4f46f7e2001d3d0b71be15cc8099bebf5ef8
Park-yebin <81156273+Park-yebin@users.noreply.github.com>
2022-11-06 16:34:09 +0900

    Setting
    get packages

---
+++ house_price.py
@@ -0,0 +1,11 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+from scipy.stats import norm
+from sklearn.preprocessing import StandardScaler
+from scipy import stats
+import warnings
+
+warnings.filterwarnings('ignore')
+# %matplotlib inline
